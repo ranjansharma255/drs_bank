17ba12d745a79446db1995fdcfc0032cf3eadff6
Ranjan Sharma <ranjansharma255@gmail.com>
2021-07-05 11:01:14 +0530

    modified navbar file

--- php/navbar.php
+++ php/navbar.php
@@ -5,6 +5,8 @@
         <nav id = "nav-bar">
             <ul class = "nav-link">
                 <li><a class = "link" href="index.php">Home</a></li>
+                <li><a class = "link" href="customers.php">Customers</a></li>               
+                <li><a class = "link" href="transactions.php">Transactions</a></li>               
                 <li><a class = "link" href="aboutme.php">About Me</a></li>               
             </ul>
         </nav>
