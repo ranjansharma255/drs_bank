c02d1e7cbc1a71f2eb91c0ad0d431cf32e243d91
Ranjan Sharma <ranjansharma255@gmail.com>
2021-07-05 12:01:57 +0530

    Changed few fields in config file

--- config.php
+++ config.php
@@ -2,9 +2,10 @@
 
 	$servername = 'localhost';
 	$username = 'root';
-	$password = '';
-	$database = 'drsbank';
+	$password = 'GgJJ8h1RbSfOmqp5';
+	$database = 'id17190719_drsbank';
 
 	$conn = mysqli_connect($servername, $username, $password, $database) or die("Connection With the database is Unsuccessful".mysqli_connect_error());
 
+	// bo^CX9\rpW6z*r{- db password
 ?>
